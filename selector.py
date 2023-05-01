ed34fdc87cf6ef9d6f7c6be812aa8a1faba2d3d9
JhsongTest <nn0221@naver.com>
2023-05-01 14:43:21 +0900

    feat: Done

--- selector.py
+++ selector.py
@@ -1,9 +1,9 @@
 import random
 
-#result = random.sample(range(1, 46), k = 6)
-
-def getLottoNum():
-    return random.sample(range(1, 46), k = 6)
+def get_lucky_nums():
+    return random.sample(range(1,45+1), k=6)
 
 if __name__ == '__main__':
-    print(getLottoNum())
+    times = int(input('Enter num(1-100): '))
+    for i in range(times):
+        print(get_lucky_nums())
